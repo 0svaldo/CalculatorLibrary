9029cb90ff66c61ddd4e011cad97cd47294930f9
Osvaldo Larancuent <osvaldo.larancuent@gmail.com>
2019-01-27 09:59:06 -0400

    primer commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""
+Libreria para el calculo de operaciones matematicas basicas
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def substract(first_term, second_term):
+    return first_term - second_term
+
+def times(first_term, second_term):
+    return first_term * second_term
+
+def divide(first_term, second_term):
+    return first_term / second_term
+
